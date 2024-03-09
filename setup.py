87b61a65efd8b637003700b53e0ab433d009bc8d
garunAMT <21ME02022@iitbbs.ac.in>
2024-03-10 00:23:44 +0530

    made imports and installed libraries

--- setup.py
+++ setup.py
@@ -1,1 +1,12 @@
 # we use setup.py files for installing the local package in our virtual environment
+
+from setuptools import setup, find_packages
+
+setup(
+    name='mcqgenerator',
+    version='0.0.1',
+    author='Anurag Mani Tripathi',
+    author_email="anuragmani000@gmail.com",
+    install_requires=["openai", "langchain", "streamlit", "python-dotenv", "PyPDF2"],
+    packages=find_packages() # responsible for finding all the local packages in the directory
+)
